adaac838b378eaf7d4c79025f44c04368087634b
NeggFriedRice <tomloo222@gmail.com>
2024-03-06 14:51:17 +1100

    Add modal for about info

---
+++ src/components/AboutMoreModal.jsx
@@ -0,0 +1,57 @@
+// Modal borrowed from Creative Tim Tailwind Starter Kit (https://www.creative-tim.com/)
+
+import React from 'react'
+
+export default function AboutMoreModal() {
+    const [showModal, setShowModal] = React.useState(false);
+  return (
+    <>
+      <button
+        className="bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+        type="button"
+        onClick={() => setShowModal(true)}
+      >
+        A bit more info please...
+      </button>
+      {showModal ? (
+        <>
+          <div
+            className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+          >
+            <div className="w-[80vw] relative my-6 mx-auto max-w-3xl">
+              {/*content*/}
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-teal-900 outline-none focus:outline-none">
+                {/*header*/}
+                <div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
+                  <h3 className="text-3xl font-semibold">
+                    My life story...
+                  </h3>
+                  <button
+                    className="p-1 ml-auto bg-transparent border-0 text-black float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                    onClick={() => setShowModal(false)}
+                  >
+                    <span className="bg-transparent text-white h-6 w-6 text-2xl block">
+                    ⓧ
+                    </span>
+                  </button>
+                </div>
+                {/*body*/}
+                <div className="relative p-6 flex-auto">
+                  <p className="my-4 text-blueGray-500 text-lg leading-relaxed font-light">
+                    Let's jump back to 6 years ago when I moved from Melbourne, Australia to Hong Kong.
+
+                    It might've been the 
+                  </p>
+                </div>
+                {/*footer*/}
+
+              </div>
+            </div>
+          </div>
+          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+        </>
+      ) : null}
+    </>
+  )
+}
+
