06344e98dc340202c0e42a07021c11cbac882b79
NeggFriedRice <tomloo222@gmail.com>
2024-03-12 21:13:20 +1100

    Redeploy for production version on Vercel

--- src/components/modals/CARtificateModal.jsx
+++ src/components/modals/CARtificateModal.jsx
@@ -42,7 +42,7 @@ export default function CARtificateModal() {
                   {/*header*/}
                   <div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
                     <h3 className="text-3xl font-semibold">
-                      CARTificate
+                      CARtificate
                     </h3>
                     <button
                       className="p-1 ml-auto bg-transparent border-0 text-black float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
