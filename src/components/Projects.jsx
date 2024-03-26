c610afaedec748136756d065ee496dab79cdccab
NeggFriedRice <tomloo222@gmail.com>
2024-03-26 12:24:55 +1100

    Change order of projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -20,6 +20,28 @@ export default function Projects() {
             </Reveal>
             <div className="xl:w-[55vw] xxl:w-[50vw] lg:w-[70vw] w-[80vw] h-[1px] bg-white rounded-sm"></div>
             <div className="flex flex-wrap justify-center m-4 lg:grid lg:grid-cols-2">
+              <div className="flex justify-center items-center">
+                <div className="flex justify-center items-center my-4">
+                  <video autoPlay loop muted src="/videos/cartificate-vid2.mp4" width="300" height="300" className="shadow-block-md"/>
+                </div>
+              </div>
+              <div className="m-2 lg:m-8">
+                <h3 className="text-center lg:text-left font-['Lilita_One'] text-[2rem] lg:text-[3rem]">CARtificate</h3>
+                <p className="text-[1.5rem] max-xs:text-[1rem]">A fully-responsive simple-to-use web app to keep track of all your car maintenance.</p>
+                <CARtificateModal />
+                <div className="animate-float duration-1.35s">
+                  <div className={`${iconHolderStyles} flex justify-center bg-blue-700 hover:bg-blue-800 w-[300px] lg:w-[400px] transition duration-2s my-4 mx-auto lg:mx-0`}>
+                      <img src="/html.svg" className={`${smallIcon} ${largeIcon}`}/>
+                      <img src="/css.svg" className={`${smallIcon} ${largeIcon}`}/>
+                      <img src="/tailwindcss.svg" className={`${smallIcon} ${largeIcon}`}/>
+                      <img src="/js.svg" className={`${smallIcon} ${largeIcon}`}/>
+                      <img src="/react.svg" className={`${smallIcon} ${largeIcon}`}/>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="w-[80vw] lg:w-[60vw] xxl:w-[50vw] h-[1px] bg-sky-400 rounded-sm"></div>
+            <div className="flex flex-wrap justify-center m-4 lg:grid lg:grid-cols-2">
               <div className="flex justify-center items-center my-4">
                 <video autoPlay loop muted src="/videos/portfolio-vid.mp4" width="300" height="300" className="shadow-block-md"/>
               </div>
@@ -58,28 +80,6 @@ export default function Projects() {
                       <img src="/express.svg" className={`${smallIcon} ${largeIcon}`}/>
                       <img src="/nodejs.svg" className={`${smallIcon} ${largeIcon}`}/>
                       <img src="/mongoose.svg" className={`${smallIcon} ${largeIcon}`}/>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="w-[80vw] lg:w-[60vw] xxl:w-[50vw] h-[1px] bg-sky-400 rounded-sm"></div>
-            <div className="flex flex-wrap justify-center m-4 lg:grid lg:grid-cols-2">
-              <div className="flex justify-center items-center">
-                <div className="flex justify-center items-center my-4">
-                  <video autoPlay loop muted src="/videos/cartificate-vid2.mp4" width="300" height="300" className="shadow-block-md"/>
-                </div>
-              </div>
-              <div className="m-2 lg:m-8">
-                <h3 className="text-center lg:text-left font-['Lilita_One'] text-[2rem] lg:text-[3rem]">CARtificate</h3>
-                <p className="text-[1.5rem] max-xs:text-[1rem]">A work in progress. A simple-to-use web app to keep track of all your car maintenance.</p>
-                <CARtificateModal />
-                <div className="animate-float duration-1.35s">
-                  <div className={`${iconHolderStyles} flex justify-center bg-blue-700 hover:bg-blue-800 w-[300px] lg:w-[400px] transition duration-2s my-4 mx-auto lg:mx-0`}>
-                      <img src="/html.svg" className={`${smallIcon} ${largeIcon}`}/>
-                      <img src="/css.svg" className={`${smallIcon} ${largeIcon}`}/>
-                      <img src="/tailwindcss.svg" className={`${smallIcon} ${largeIcon}`}/>
-                      <img src="/js.svg" className={`${smallIcon} ${largeIcon}`}/>
-                      <img src="/react.svg" className={`${smallIcon} ${largeIcon}`}/>
                   </div>
                 </div>
               </div>
