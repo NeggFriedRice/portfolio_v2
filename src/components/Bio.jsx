83070379e5b11ba7ce90f718e871d99ba69aa32f
NeggFriedRice <tomloo222@gmail.com>
2024-03-26 20:29:10 +1100

    Add delay to intro text more suited to delayed animation of name being shown

--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -16,13 +16,13 @@ export default function Bio() {
             <motion.div
               initial={{ x: 80, opacity: 0}}
               animate={{ x: 0, opacity: 1}}
-              transition={{ duration: 1.5, delay: 0.75, type: "spring", stiffness: 80 }}>
+              transition={{ duration: 1.5, delay: 1.1, type: "spring", stiffness: 80 }}>
               <h1 className="text-[26px] font-['Poppins'] rotate-90 relative top-[70px] right-[-132px] text-white whitespace-break-spaces max-xs:right-[-100px] max-xs:text-[20px] max-xs:top-[62px]">Juni     r Full Stack Developer</h1>
             </motion.div>
             <motion.div
               initial={{ opacity: 0, y: 50 }}
               animate={{ opacity: 1, y: 0 }}
-              transition={{ duration: 1.5, delay: 0.75, type: "spring", stiffness: 80 }}>
+              transition={{ duration: 1.5, delay: 1.1, type: "spring", stiffness: 80 }}>
               <img src="profile_pic3.png" className="relative top-[-20px] right-[10px] shadow-block-md hover:shadow-block-lg hover:scale-105 hover:rounded-lg transition-all ease-in-out delay-100 duration-300 aspect-square overflow-hidden max-xs:h-[200px] max-xs:right-[-20px]"/>
             </motion.div>
             </div>
