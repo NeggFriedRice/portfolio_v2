68f07bc70ffce5ce8954e3317597f3ac4855ae07
NeggFriedRice <tomloo222@gmail.com>
2024-03-28 14:16:30 +1100

    Update footer wording

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
   return (
     <div className="flex justify-center">
-      <p className="bottom-0 text-['Poppins'] text-[14px] font-light text-violet-400">Crafted and designed with ❤ by Thomas Loo</p>
+      <p className="bottom-0 text-['Poppins'] text-[14px] font-light text-violet-400">Thomas Loo 2024</p>
     </div>
     
   )
