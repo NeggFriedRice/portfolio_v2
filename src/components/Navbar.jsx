1dac80129ac637db2ce37bdcba06cd1d9038bc96
Thomas Loo <tomloo222@gmail.com>
2024-04-27 00:33:04 +1000

    Add line animation to navbar buttons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,19 +9,11 @@ export default function Navbar() {
             initial={{ opacity: 0 }}
             animate={{ opacity: 1, transition: {delay: 3, duration: 1.5}}}
             className="flex absolute w-auto text-[1rem] lg:text-[25px] text-white p-4">
-                <li className="mx-4 lg:mx-12">
-                    <motion.a 
-                    href="#about"
-                    whileHover={{ opacity: 0.5 }}>
-                    About
-                    </motion.a>
+                <li className="navbar-button mx-4 lg:mx-12">
+                    <a href="#about">About</a>
                 </li>
-                <li className="mx-4 lg:mx-12">
-                    <motion.a 
-                    href="#projects"
-                    whileHover={{ opacity: 0.5 }}>
-                    Projects
-                    </motion.a>
+                <li className="navbar-button mx-4 lg:mx-12">
+                    <a href="#projects">Projects</a>
                 </li>
             </motion.ul>
         </div>
