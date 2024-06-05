ea80f0c7053c6882f492421e24c657bd9618d84d
Thomas Loo <tomloo222@gmail.com>
2024-06-06 00:25:58 +1000

    Add alt text to icons in Stack component

--- src/components/Stack.jsx
+++ src/components/Stack.jsx
@@ -21,16 +21,16 @@ export default function Stack() {
           <div className="flex py-6 justify-center">
               {/* <div className="w-[60vw] px-4 grid grid-cols-3 place-items-center lg:flex lg:justify-center"> */}
                 <PopReveal>
-                  <motion.img variants={imgVariants} key="html" src="/html.svg" className={`${stackStyle} duration-1.9s`} />
-                  <motion.img variants={imgVariants} key="css" src="/css.svg" className={`${stackStyle} duration-1.75s`}/>
-                  <motion.img variants={imgVariants} key="tailwind" src="/tailwindcss.svg" className={`${stackStyle} duration-1.5s`}/>
-                  <motion.img variants={imgVariants} key="js" src="/js.svg" className={`${stackStyle} duration-1.35s`}/>
-                  <motion.img variants={imgVariants} key="node" src="/nodejs.svg" className={`${stackStyle} duration-1.55s`}/>
-                  <motion.img variants={imgVariants} key="react" src="/react.svg" className={`${stackStyle} duration-1.6s`}/>
-                  <motion.img variants={imgVariants} key="express" src="/express.svg" className={`${stackStyle} duration-1.8s`}/>
-                  <motion.img variants={imgVariants} key="mongoose" src="/mongoose.svg" className={`${stackStyle} duration-1.95s`}/>
-                  <motion.img variants={imgVariants} key="mongodb" src="/mongodb.svg" className={`${stackStyle} duration-1.25s`}/>
-                  <motion.img variants={imgVariants} key="python" src="/python.svg" className={`${stackStyle} duration-1.55s`}/>
+                  <motion.img variants={imgVariants} alt="html logo" key="html" src="/html.svg" className={`${stackStyle} duration-1.9s`} />
+                  <motion.img variants={imgVariants} alt="css logo" key="css" src="/css.svg" className={`${stackStyle} duration-1.75s`}/>
+                  <motion.img variants={imgVariants} alt="tailwind css logo" key="tailwind" src="/tailwindcss.svg" className={`${stackStyle} duration-1.5s`}/>
+                  <motion.img variants={imgVariants} alt="javascript logo" key="js" src="/js.svg" className={`${stackStyle} duration-1.35s`}/>
+                  <motion.img variants={imgVariants} alt="nodejs logo" key="node" src="/nodejs.svg" className={`${stackStyle} duration-1.55s`}/>
+                  <motion.img variants={imgVariants} alt="react logo" key="react" src="/react.svg" className={`${stackStyle} duration-1.6s`}/>
+                  <motion.img variants={imgVariants} alt="express logo" key="express" src="/express.svg" className={`${stackStyle} duration-1.8s`}/>
+                  <motion.img variants={imgVariants} alt="mongoose logo" key="mongoose" src="/mongoose.svg" className={`${stackStyle} duration-1.95s`}/>
+                  <motion.img variants={imgVariants} alt="mongodb logo" key="mongodb" src="/mongodb.svg" className={`${stackStyle} duration-1.25s`}/>
+                  <motion.img variants={imgVariants} alt="logo" key="python" src="/python.svg" className={`${stackStyle} duration-1.55s`}/>
                 </PopReveal>
               {/* </div> */}
           </div>
