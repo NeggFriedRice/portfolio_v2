ea533c35a89c3aa19e2d669bcc121c9427678e0f
Thomas Loo <tomloo222@gmail.com>
2024-07-28 19:50:53 +1000

    Update timeline info

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -21,12 +21,12 @@ function Timeline() {
       }
       >
         <h3 className="vertical-timeline-element-title text-[1.5rem] pb-2">Junior Full Stack Developer</h3>
-        <h4 className="vertical-timeline-element-subtitle text-[1.25rem] pb-2">ResearchSat</h4>
+        <h4 className="vertical-timeline-element-subtitle text-[1.25rem] pb-2 font-bold">ResearchSat</h4>
         <div>
           <ul className="list-disc text-[1.25rem] px-4">
             <li className="py-2">Developed a full stack sensor data dashboard with React, Node.js/Express, HTML and styled with Tailwind CSS; enabling ResearchSat to present live feed mission data to clients</li>
             <li  className="py-2">Implemented back end Node.js pipeline to read and save raw data from AWS S3 to MongoDB</li>
-            <li className="py-2">Created 6 RESTful APIs to server sensor data from MongoDB to front end</li>
+            <li className="py-2">Created 6 RESTful APIs to serve sensor data from MongoDB to front end</li>
             <li className="py-2">Integrated dynamic and interactable data visualisations (temperature, humidity, pressure and spectral data) by utilising Recharts library</li>
           </ul>
         </div>
@@ -44,12 +44,11 @@ function Timeline() {
       }
       >
         <h3 className="vertical-timeline-element-title text-[1.5rem] pb-2">Business Analyst</h3>
-        <h4 className="vertical-timeline-element-subtitle text-[1.25rem] pb-2">HSBC</h4>
+        <h4 className="vertical-timeline-element-subtitle text-[1.25rem] pb-2 font-bold">HSBC</h4>
         <div>
           <ul className="list-disc text-[1.25rem] px-4">
-            <li className="py-2">Managed a sector wide SharePoint site built with HTML and CSS</li>
-            <li  className="py-2">Engaged as a Business Analyst to focus on change management and process improvement for the Direct Custody & Clearing (commercial) product operations</li>
-            <li className="py-2">Automated weekly stakeholder report using Excel, SharePoint and VBA reducing a 2 hour task to 15 seconds</li>
+            <li className="py-2">At HSBC I had the opportunity to work on some exciting projects including looking after a sector-wide SharePoint which involved learning HTML and CSS to create an engaging space for colleagues</li>
+            <li className="py-2">I was also working in the capacity as a change manager for the Direct Custody & Clearing product, researching and testing new ways to increase efficiency in a range of processes</li>
           </ul>
         </div>
       </VerticalTimelineElement>
@@ -57,7 +56,7 @@ function Timeline() {
         className="vertical-timeline-element--work"
         contentStyle={{ background: "rgb(2, 38, 64)", color: "#fff" }}
         contentArrowStyle={{ borderRight: "7px solid  rgb(2, 38, 64)" }}
-        date="September 2020 to June 2023"
+        date="June 2019 to May 2020"
         iconStyle={{ background: "rgb(2, 38, 64)", color: "#fff" }}
         icon={
           <div className="h-[100%] flex justify-center place-items-center">
@@ -66,11 +65,10 @@ function Timeline() {
       }
       >
         <h3 className="vertical-timeline-element-title text-[1.5rem] pb-2">Business Analyst</h3>
-        <h4 className="vertical-timeline-element-subtitle text-[1.25rem] pb-2">AXA Group Solutions</h4>
+        <h4 className="vertical-timeline-element-subtitle text-[1.25rem] pb-2 font-bold">AXA Group Solutions</h4>
         <div>
           <ul className="list-disc text-[1.25rem] px-4">
-            <li className="py-2">Engaged as a business analyst and provided a customer facing chat bot dataanalysis and solution (mobile and web-based app) that resulted in an accuracy increase of over 40%.</li>
-            <li  className="py-2">Contributed to a chat bot operating model through process mapping, gap analysis and identifying critical success factors</li>
+            <li className="py-2">At AXA I was part of the digital transformation team working on a customer facing chat bot project; through data analysis and testing I put forward a solution that raised the chat bot's response accuracy from 50% to over 80%</li>
           </ul>
         </div>
       </VerticalTimelineElement>
