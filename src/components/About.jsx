6caec504e6383c066edffea082c5a1eba8397d1d
Thomas Loo <tomloo222@gmail.com>
2024-07-28 16:40:16 +1000

    Add timeline to About section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,36 +1,60 @@
-import React from 'react'
-import AboutMoreModal from './modals/AboutMoreModal'
-import Reveal from '../utils/Reveal'
+import React from "react";
+import AboutMoreModal from "./modals/AboutMoreModal";
+import Reveal from "../utils/Reveal";
+import Timeline from "./Timeline";
 
 export default function About() {
   return (
     <>
-        <section className="font-light flex justify-center px-6 bg-gray-800 text-white text-[clamp(16px,2vw,36px)] py-[5vh]">
-            <div className="xl:w-[50vw] lg:w-[70vw] w-[100vw] xxl:w-[40vw]">
-                <div className="flex justify-items-end justify-end px-16">
-                <Reveal direction={"left"} delay={0.35}>
-                    <h1 className="inline text-[clamp(26px,10vw,48px)] font-semibold">About</h1>
-                </Reveal>
-                </div>
-                <div className="flex justify-items-end justify-end">
-                    <div className="w-[83vw] lg:w-[70vw] h-[1px] bg-white rounded-sm mx-12"></div>
-                </div>
-                <div className="py-6 flex justify-center">
-                    <div className="w-[80vw] md:grid md:grid-cols-2 flex flex-wrap justify-center">
-                        <p className="py-2 col-start-1 col-span-2 text-[1.5rem] max-xs:text-[1rem]">
-                        Hi there! I'm Tom! I used to be a business analyst but now I'm on a pursuit of curiosity to build front ends and back ends and everything in between!<br></br><br></br> I'm a bright-eyed 👀, squash-playing 🎾, car-enthusiast 🚗 full stack web developer that likes tinkering with code and creating something <em>a little bit special</em>.</p>
-                        <div>
-                            <p className="py-2 text-[1.5rem] max-xs:text-[1rem] lg:mb-8">With experience in the MERN stack, HTML, CSS, JavaScript and a sprinkle of Python, I'm passionate about building engaging, interesting and fun apps!</p>
-                            {/* <AboutMoreModal /> */}
-                        </div>
-                        <div className="grid grid-cols-12 grid-rows-12 h-48 w-48 relative justify-center justify-self-center z-10">
-                            <img alt="Cartoon image of a person sitting at a computer" src="computer.png" className="col-span-8 col-start-3 row-start-6 max-h-36 max-w-72"></img>
-                            <img alt="Cut out of Thomas Loo's head in sticker style" src="stickerhead.png" className="col-span-6 col-start-8 row-start-1 animate-bobble origin-bottom hover:animate-bobbleFast transition-all max-h-27 max-w-24"></img>
-                        </div>
-                    </div>
-                </div>
+      <section className="font-light flex justify-center px-6 bg-gray-800 text-white text-[clamp(16px,2vw,36px)] py-[5vh]">
+        <div className="xl:w-[50vw] lg:w-[70vw] w-[100vw] xxl:w-[40vw]">
+          <div className="flex justify-items-end justify-end px-16">
+            <Reveal direction={"left"} delay={0.35}>
+              <h1 className="inline text-[clamp(26px,10vw,48px)] font-semibold">
+                About
+              </h1>
+            </Reveal>
+          </div>
+          <div className="flex justify-items-end justify-end">
+            <div className="w-[83vw] lg:w-[70vw] h-[1px] bg-white rounded-sm mx-12"></div>
+          </div>
+          <div className="py-6 flex justify-center">
+            <div className="w-[80vw] md:grid md:grid-cols-2 flex flex-wrap justify-center">
+              <p className="pb-4 col-start-1 col-span-2 text-[1.5rem] max-xs:text-[1rem]">
+                Hi there! I'm Tom! I used to be a business analyst but now I'm
+                on a pursuit of curiosity to build front ends and back ends and
+                everything in between!
+              </p>
+              <p className="pb-4 col-start-1 col-span-2 text-[1.5rem] max-xs:text-[1rem]">
+                I'm a bright-eyed 👀, squash-playing 🎾, car-enthusiast 🚗 full
+                stack web developer that likes tinkering with code and creating
+                something <em>a little bit special</em>.
+              </p>
+              <p className="text-[1.5rem] max-xs:text-[1rem] lg:mb-8">
+                With experience in the MERN stack, HTML, CSS, JavaScript and a
+                sprinkle of Python, I'm passionate about building engaging,
+                interesting and fun apps!
+              </p>
+              {/* <AboutMoreModal /> */}
+              <div className="grid grid-cols-12 grid-rows-12 h-48 w-48 relative justify-center justify-self-center z-10">
+                <img
+                  alt="Cartoon image of a person sitting at a computer"
+                  src="computer.png"
+                  className="col-span-8 col-start-3 row-start-6 max-h-36 max-w-72"
+                ></img>
+                <img
+                  alt="Cut out of Thomas Loo's head in sticker style"
+                  src="stickerhead.png"
+                  className="col-span-6 col-start-8 row-start-1 animate-bobble origin-bottom hover:animate-bobbleFast transition-all max-h-27 max-w-24"
+                ></img>
+              </div>
             </div>
-        </section>
+          </div>
+          <div className="pt-12">
+            <Timeline />
+          </div>
+        </div>
+      </section>
     </>
-  )
+  );
 }
---
+++ src/components/Timeline.jsx
@@ -0,0 +1,81 @@
+import React from "react";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+
+function Timeline() {
+  return (
+    <VerticalTimeline>
+      <VerticalTimelineElement
+        className="vertical-timeline-element--work"
+        contentStyle={{}}
+        contentArrowStyle={{ borderRight: "7px solid  rgb(2, 38, 64)" }}
+        date="May 2024 to July 2024"
+        iconStyle={{ background: "rgb(2, 38, 64)", color: "#fff" }}
+        icon={
+          <div className="h-[100%] flex justify-center place-items-center">
+        <img src="/RSLogo.png" className="w-[40px]"/>
+        </div>
+      }
+      >
+        <h3 className="vertical-timeline-element-title text-[1.5rem] pb-2">Junior Full Stack Developer</h3>
+        <h4 className="vertical-timeline-element-subtitle text-[1.25rem] pb-2">ResearchSat</h4>
+        <div>
+          <ul className="list-disc text-[1rem] px-4">
+            <li className="py-2">Developed a full stack sensor data dashboard with React, Node.js/Express, HTML and styled with Tailwind CSS; enabling ResearchSat to present live feed mission data to clients</li>
+            <li  className="py-2">Implemented back end Node.js pipeline to read and save raw data from AWS S3 to MongoDB</li>
+            <li className="py-2">Created 6 RESTful APIs to server sensor data from MongoDB to front end</li>
+            <li className="py-2">Integrated dynamic and interactable data visualisations (temperature, humidity, pressure and spectral data) by utilising Recharts library</li>
+          </ul>
+        </div>
+      </VerticalTimelineElement>
+      <VerticalTimelineElement
+        className="vertical-timeline-element--work"
+        contentStyle={{ background: "rgb(2, 38, 64)", color: "#fff" }}
+        contentArrowStyle={{ borderRight: "7px solid  rgb(2, 38, 64)" }}
+        date="September 2020 to June 2023"
+        iconStyle={{ background: "rgb(2, 38, 64)", color: "#fff" }}
+        icon={
+          <div className="h-[100%] flex justify-center place-items-center">
+        <img src="/HSBCLogo.png" className="w-[40px]"/>
+        </div>
+      }
+      >
+        <h3 className="vertical-timeline-element-title text-[1.5rem] pb-2">Business Analyst</h3>
+        <h4 className="vertical-timeline-element-subtitle text-[1.25rem] pb-2">HSBC</h4>
+        <div>
+          <ul className="list-disc text-[1rem] px-4">
+            <li className="py-2">Managed a sector wide SharePoint site built with HTML and CSS</li>
+            <li  className="py-2">Engaged as a Business Analyst to focus on change management and process improvement for the Direct Custody & Clearing (commercial) product operations</li>
+            <li className="py-2">Automated weekly stakeholder report using Excel, SharePoint and VBA reducing a 2 hour task to 15 seconds</li>
+          </ul>
+        </div>
+      </VerticalTimelineElement>
+      <VerticalTimelineElement
+        className="vertical-timeline-element--work"
+        contentStyle={{ background: "rgb(2, 38, 64)", color: "#fff" }}
+        contentArrowStyle={{ borderRight: "7px solid  rgb(2, 38, 64)" }}
+        date="September 2020 to June 2023"
+        iconStyle={{ background: "rgb(2, 38, 64)", color: "#fff" }}
+        icon={
+          <div className="h-[100%] flex justify-center place-items-center">
+        <img src="/AXALogo.jpg" className="w-[40px]"/>
+        </div>
+      }
+      >
+        <h3 className="vertical-timeline-element-title text-[1.5rem] pb-2">Business Analyst</h3>
+        <h4 className="vertical-timeline-element-subtitle text-[1.25rem] pb-2">AXA Group Solutions</h4>
+        <div>
+          <ul className="list-disc text-[1rem] px-4">
+            <li className="py-2">Engaged as a business analyst and provided a customer facing chat bot dataanalysis and solution (mobile and web-based app) that resulted in an accuracy increase of over 40%.</li>
+            <li  className="py-2">Contributed to a chat bot operating model through process mapping, gap analysis and identifying critical success factors</li>
+          </ul>
+        </div>
+      </VerticalTimelineElement>
+    </VerticalTimeline>
+  );
+}
+
+export default Timeline;
