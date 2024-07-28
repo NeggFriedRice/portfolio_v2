5ab7c2c9b61745d78f88b9b638db0d5fdebe7807
Thomas Loo <tomloo222@gmail.com>
2024-07-28 17:16:24 +1000

    Increase font size to 1.25rem for timeline text

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -23,7 +23,7 @@ function Timeline() {
         <h3 className="vertical-timeline-element-title text-[1.5rem] pb-2">Junior Full Stack Developer</h3>
         <h4 className="vertical-timeline-element-subtitle text-[1.25rem] pb-2">ResearchSat</h4>
         <div>
-          <ul className="list-disc text-[1rem] px-4">
+          <ul className="list-disc text-[1.25rem] px-4">
             <li className="py-2">Developed a full stack sensor data dashboard with React, Node.js/Express, HTML and styled with Tailwind CSS; enabling ResearchSat to present live feed mission data to clients</li>
             <li  className="py-2">Implemented back end Node.js pipeline to read and save raw data from AWS S3 to MongoDB</li>
             <li className="py-2">Created 6 RESTful APIs to server sensor data from MongoDB to front end</li>
@@ -46,7 +46,7 @@ function Timeline() {
         <h3 className="vertical-timeline-element-title text-[1.5rem] pb-2">Business Analyst</h3>
         <h4 className="vertical-timeline-element-subtitle text-[1.25rem] pb-2">HSBC</h4>
         <div>
-          <ul className="list-disc text-[1rem] px-4">
+          <ul className="list-disc text-[1.25rem] px-4">
             <li className="py-2">Managed a sector wide SharePoint site built with HTML and CSS</li>
             <li  className="py-2">Engaged as a Business Analyst to focus on change management and process improvement for the Direct Custody & Clearing (commercial) product operations</li>
             <li className="py-2">Automated weekly stakeholder report using Excel, SharePoint and VBA reducing a 2 hour task to 15 seconds</li>
@@ -68,7 +68,7 @@ function Timeline() {
         <h3 className="vertical-timeline-element-title text-[1.5rem] pb-2">Business Analyst</h3>
         <h4 className="vertical-timeline-element-subtitle text-[1.25rem] pb-2">AXA Group Solutions</h4>
         <div>
-          <ul className="list-disc text-[1rem] px-4">
+          <ul className="list-disc text-[1.25rem] px-4">
             <li className="py-2">Engaged as a business analyst and provided a customer facing chat bot dataanalysis and solution (mobile and web-based app) that resulted in an accuracy increase of over 40%.</li>
             <li  className="py-2">Contributed to a chat bot operating model through process mapping, gap analysis and identifying critical success factors</li>
           </ul>
